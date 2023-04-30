86e38a49427eb8c6a3f6af11a82ad4851d9b7070
stanis-tsv <stanis-tsv@gmail.com>
2023-04-30 20:48:01 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-app-form-with-steps/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/vue-app-form-with-steps/'
+//     : '/'
+// }
